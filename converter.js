3b57b0baa48dcb3ffea6316c6f71e714facd384f
katherine tuter <katherinetuter@gmail.com>
2017-01-28 14:02:22 -0600

    figuring out how to convert temps

--- converter.js
+++ converter.js
@@ -1,11 +1,14 @@
+convertedTemp.onclick();
 
 //convert temperature from F to C
 function toCelsius () {
-
+	var toC = [t] - 32
+/ 1.8;
+convertedTemp.onclick();
 }
 //convert temperature from C to F
 function toFahrenheit () {
-
+	var toF = [t] * 1.8 + 32;
 }
 
 // Get a reference to the button element in the DOM
