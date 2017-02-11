acb8f02e2dd32a66098b162ea2744dcde0bd1613
katherine tuter <katherinetuter@gmail.com>
2017-02-11 12:52:46 -0600

    added clear on click functionality to clear button

--- converter.js
+++ converter.js
@@ -1,12 +1,20 @@
 //Get a reference to the button element in the DOM & assign function
 var button = document.getElementById("converter");
 button.addEventListener("click", determineConverter);
+//set the clear button to clear input field
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearText);
+//determine if enter is pressed
 document.querySelector("#text").addEventListener("keyPress", function (e) {
 	var key = e.which || e.keyCode;
 	if (key === 13) {
 		determineConverter();
 	}
 });
+
+function clearText () {
+	document.getElementById("text").value="";
+}
 
 function toCelsius (x) {
  	return (x - 32) / 1.8;
@@ -19,14 +27,12 @@ function toFahrenheit (x) {
 function determineConverter () {
  if(document.getElementById("fahrenheit").checked) {
 	var farTemp = document.getElementById("text").value; //get F value from input
-	document.getElementById("text").value=""; //clear F
 	document.getElementById("fahrenheit").value;
 	var cAnswer = toCelsius(farTemp); //call toCelsius function
 	document.getElementById("answer").innerHTML = "<p>" + cAnswer + " &deg; Celsius</p>"; //write answer to html
 	}
 	else if (document.getElementById("celsius").checked) {
 	var celTemp = document.getElementById("text").value; //get C value from input
-	document.getElementById("text").value=""; //clear C
 	var cToF = document.getElementById("celsius").value;
 	var fAnswer = toFahrenheit(celTemp); //call toFahrenheit function
 	document.getElementById("answer").innerHTML = "<p>" + fAnswer + " &deg; Fahrenheit</p>"; //write answer to html
